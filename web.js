f9f02609b161c45f691a7407e16d981abda763f0
Ubuntu <ubuntu@ip-172-31-5-84.us-west-2.compute.internal>
2013-07-10 03:42:36 +0000

    Require fs before using it

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buffer = new Buffer(fs.readFileSync('index.html'));
   response.send(buffer.toString('utf-8'));
 });
